f3ce27c43ffd1515ddaf7494a7926e0cd2c8bc0c
Pratham Chauhan <prathamchauhan.coding42@gmail.com>
2023-08-01 18:11:09 +0530

    working again

--- otipy_scrape.py
+++ otipy_scrape.py
@@ -14,40 +14,60 @@ else:
     FD = pd.DataFrame(columns=column)
 
 
-headers = {
-    'authority': 'gcptest.crofarm.com',
-    'accept': '*/*',
-    'accept-language': 'en-US,en;q=0.9,hi;q=0.8',
-    'access-token': 'e31306fb27e6172c8be9672ea7e95ee5a7eccb7a9b788d282d65a56ed91c3878',
-    'client': 'web',
-    'content-type': 'application/json',
-    'dnt': '1',
-    'origin': 'https://www.otipy.com',
-    'referer': 'https://www.otipy.com/',
-    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'cross-site',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-}
+def extract_info(i):
+    price = i['price']  # Limited Price
 
-params = {
-    'page': '1',
-    'feed_type': 'web',
-    'latitude': '28.45170162',
-    'longitude': '77.01849746',
-}
+    start_time, end_time = i['start_time'], i['end_time']
+    start_time_string = datetime.fromtimestamp(start_time)
+    end_time_string = datetime.fromtimestamp(end_time)
+
+    prod = i['normal_product']
+    id = prod['id']
+
+    # pdb.set_trace()
+    if not FD.empty:
+        for row in FD.to_numpy():
+            if (id == row[0]) & (start_time == row[1]):
+                print('item already stored.', (id, start_time))
+                return
+
+    name, quantity, normal_price = prod['name'], prod['pack_qt'], prod['price']
+
+    d = [id, start_time, end_time, start_time_string, end_time_string,
+         name, quantity, price, normal_price, (normal_price-price)]
+    # print("{:35s} | {:10s} | ₹{:5d} | ₹{:5d}".format(*d))
+    FD.loc[len(FD)] = d
 
 
-# pdb.set_trace()
+def scrape():
+    headers = {
+        'authority': 'gcptest.crofarm.com',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9,hi;q=0.8',
+        'access-token': 'e31306fb27e6172c8be9672ea7e95ee5a7eccb7a9b788d282d65a56ed91c3878',
+        'client': 'web',
+        'content-type': 'application/json',
+        'dnt': '1',
+        'origin': 'https://www.otipy.com',
+        'referer': 'https://www.otipy.com/',
+        'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    }
 
+    params = {
+        'page': '1',
+        'feed_type': 'web',
+        'latitude': '28.45170162',
+        'longitude': '77.01849746',
+    }
 
-def extract_info():
     x = requests.get('https://gcptest.crofarm.com/otipy/web/feed/v1/',
                      params=params, headers=headers)
-
     if x.status_code != 200:
         return
 
@@ -55,36 +75,11 @@ def extract_info():
     flash_deal = jdata[1]['data']['items']
 
     print('Flash Deal Items:', len(flash_deal))
-    # print("{:35s} | {:10s} | {:5s} | {:5s}".format(FD.columns))
-    for prod in flash_deal:
-
-        price = prod['price']
-        id = prod['prod_id']
-
-        start_time = prod['start_time']
-        start_time_string = datetime.fromtimestamp(start_time)
-        end_time = prod['end_time']
-        end_time_string = datetime.fromtimestamp(end_time)
-
-        # pdb.set_trace()
-        if not FD.empty:
-            if (id in FD['ID'].values) & (start_time in FD['Start_time'].values):
-                print('Flash deal item already stored.', (id, start_time))
-                continue
-
-        # normal product details
-        prod = prod['normal_product']
-        name = prod['name']
-        quantity = prod['pack_qt']
-        normal_price = prod['price']
-
-        d = [id, start_time, end_time, start_time_string, end_time_string,
-             name, quantity, price, normal_price, (normal_price-price)]
-        # print("{:35s} | {:10s} | ₹{:5d} | ₹{:5d}".format(*d))
-        FD.loc[len(FD)] = d
+    for item in flash_deal:
+        extract_info(item)
 
 
-extract_info()
+scrape()
 print(FD)
 
 FD.to_csv(saved_location, index=False)
